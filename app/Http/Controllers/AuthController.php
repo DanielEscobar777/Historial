c0dc4dde097ea96a78470266d00e5fafc8d236c8
DanielEscobar777 <da61137333@gmail.com>
2025-08-22 11:56:06 -0400

    v1.1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,14 +61,14 @@ class AuthController extends Controller
             'password' => 'required'
         ]);
         
-        $response = Http::post('http://localhost/tokkens/login.php', [
-            'email' => $request->email,
-            'password' => $request->password,
-        ]);
-         /*$response = Http::post('http://192.168.4.55:8000/api/auth/login_service', [
+        /*$response = Http::post('http://localhost/tokkens/login.php', [
             'email' => $request->email,
             'password' => $request->password,
         ]);*/
+         $response = Http::post('http://192.168.4.55:8000/api/auth/login_service', [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
 
         $data = $response->json();
 
@@ -100,9 +100,9 @@ class AuthController extends Controller
             'Authorization' => 'Bearer ' . $accessToken
         ])->get('http://localhost/tokkens/usuarios_residentes.php');
         
-        /*$usuariosResponse = Http::withHeaders([
+        $usuariosResponse = Http::withHeaders([
             'Authorization' => 'Bearer ' . $accessToken
-     ])->get('http://192.168.4.55:8001/api/s1/administracion/user_residente');*/
+     ])->get('http://192.168.4.55:8001/api/s1/administracion/user_residente');
 
         if ($usuariosResponse->ok()) {
             $usuariosData = $usuariosResponse->json();
