39a157315ba00df598b655c94d30ebf3086bb666
DanielEscobar777 <150990829+DanielEscobar777@users.noreply.github.com>
2026-07-09 23:52:31 -0400

    Simplify index method in UsuarioController
    Removed authorization check for 'Jefe de Enseñanza' role in index method.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -10,13 +10,9 @@ use App\Models\Servicio;
 class UsuarioController extends Controller
 {
     public function index() {
-        
-        $usuarios = User::with('roles')->get();
-         if (!auth()->user()->hasRole('Jefe de Enseñanza')) {
-        abort(403, 'Acceso no autorizado');
-    }
-        return view('usuarios.index', compact('usuarios'));
-    }
+    $usuarios = User::with('roles')->get();
+    return view('usuarios.index', compact('usuarios'));
+}
 
     public function create()
     {
