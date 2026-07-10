dd675574f282f27fe9f223c4b479c038c0b32395
DanielEscobar777 <150990829+DanielEscobar777@users.noreply.github.com>
2026-07-09 22:36:57 -0400

    Fix role name case in UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -12,7 +12,7 @@ class UsuarioController extends Controller
     public function index() {
         
         $usuarios = User::with('roles')->get();
-         if (!auth()->user()->hasRole('Jefe de enseñanza')) {
+         if (!auth()->user()->hasRole('Jefe de Enseñanza')) {
         abort(403, 'Acceso no autorizado');
     }
         return view('usuarios.index', compact('usuarios'));
